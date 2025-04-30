31c6fb1bfdf652f99d0d4367aec39348a4dcaaa3
Jenifer <mexajeni@gmail.com>
2025-04-30 09:34:20 +0530

    Update site: 2025-04-30 09:34:20

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745985227',resources: [
+    cacheName: 'chirpy-1745985860',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
