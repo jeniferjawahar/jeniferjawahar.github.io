309141cd123b5f101e8537d0ce15ff3eb06e1d6d
Jenifer <mexajeni@gmail.com>
2025-05-08 13:48:23 +0530

    “new-content”

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1746692257',resources: [
+    cacheName: 'chirpy-1746692294',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
